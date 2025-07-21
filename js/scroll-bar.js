4837bfe3730f0d860a2e49991730775016308399
Will Harrison <willharrison2016@gmail.com>
2025-07-20 22:18:09 -0500

    Delete js/scroll-bar.js

--- js/scroll-bar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", () => {
-  const scrollBar = document.getElementById("scroll-bar");
-  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrollPercent = (scrollTop / scrollHeight) * 100;
-  scrollBar.style.width = scrollPercent + "%";
-});
